707a213bdcf6c193f09d32b41070f9765215b4d5
mohitrobo <37827176+mohitrobo@users.noreply.github.com>
2018-07-05 12:09:10 +0530

    Update mpu6050accelerometer.ino

--- MPU6050/mpu6050accelerometer.ino
+++ MPU6050/mpu6050accelerometer.ino
@@ -1,9 +1,9 @@
 
 
-    #include<Wire.h>  //I2C library
+    #include<Wire.h>  
     int accelResult[3]; 
-    float timeStep = 0.02;  //20ms. need a time step value for integration of gyro angle from angle/sec
-    float biasAccelX,biasAccelY,biasAccelZ;  //for zero bias
+    float timeStep = 0.02; 
+    float biasAccelX,biasAccelY,biasAccelZ;  
     float rollAccel = 0;
     float pitchAccel = 0;
     void writeTo(byte device, byte toAddress, byte val)
@@ -22,19 +22,19 @@
       int i = 0; 
       while(Wire.available()) 
       {
-        result[i] = Wire.read(); //result stores 1 byte of data
+        result[i] = Wire.read(); 
         i++;
       }
     } 
 
-    //Function for reading the accelerometers
-    void getAccelerometerReadings(int accelResult[])   //Accelerometer data is in 2's complement form
+   
+    void getAccelerometerReadings(int accelResult[]) 
     {
       byte buffer[6];
       readFrom(0x68,0x3B,6,buffer);
-      accelResult[0] = (((int)buffer[0]) << 8 ) | buffer[1]; // X value
-      accelResult[1] = (((int)buffer[2]) << 8 ) | buffer[3]; // Y value
-      accelResult[2] = (((int)buffer[4]) << 8 ) | buffer[5]; // Z value
+      accelResult[0] = (((int)buffer[0]) << 8 ) | buffer[1]; 
+      accelResult[1] = (((int)buffer[2]) << 8 ) | buffer[3]; 
+      accelResult[2] = (((int)buffer[4]) << 8 ) | buffer[5]; 
     }
     
     void setup() 
@@ -43,8 +43,8 @@
       int totalAccelYValues = 0;
       int totalAccelZValues = 0;
       int i;  
-      Wire.begin();  //open I2C communication as master
-      Serial.begin(115200);  //open serial communication to PC
+      Wire.begin();  
+      Serial.begin(115200);  
       writeTo(0x68,0x6B,0x09); 
       writeTo(0x68,0x1C,0x00); 
       writeTo(0x68,0x1A,0x02); 
@@ -65,14 +65,10 @@
   void loop() 
   {
     getAccelerometerReadings(accelResult);
-    //calculate the roll in degrees as measured by the accelerometer
-    rollAccel = atan2((accelResult[1]-biasAccelY),(accelResult[2]-biasAccelZ)) * 57.3;  //angle calculated using inverse of tan  
-    //calculate the pitch in degrees as measured by the accelerometer
+    rollAccel = atan2((accelResult[1]-biasAccelY),(accelResult[2]-biasAccelZ)) * 57.3;   
     pitchAccel = atan2(-(accelResult[0]-biasAccelX),sqrt((accelResult[1]-biasAccelY)*(accelResult[1]-biasAccelY)+(accelResult[2]-biasAccelZ)*(accelResult[2]-biasAccelZ)))*57.3; 
-    //display roll data from accelerometer
     Serial.print("AccelRoll= ");
     Serial.print(rollAccel);
-    //display pitch data from accelerometer
     Serial.print(" AccelPitch= ");
     Serial.print(pitchAccel);
     Serial.print("\n");
